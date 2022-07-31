da08826f60becbb3e643118777d872f55f4394ba
Jason Mongrillo D�az <jmongrio@hotmail.com>
2022-07-31 15:43:55 -0600

    Add project files.

---
+++ Chat/ChatHUB.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace Chat
+{
+    public class ChatHUB : Hub
+    {
+        public async Task SendMessage(string room, string user, string message)
+        {
+            await Clients.Group(room).SendAsync("ReceiveMessage", user, message); 
+        }
+
+        public async Task AddToGroup(string room)
+        {
+            await Groups.AddToGroupAsync(Context.ConnectionId, room);
+
+            await Clients.Group(room).SendAsync("ShowWho", $"Se ha conectado {Context.ConnectionId}.");
+        }
+    }
+}
